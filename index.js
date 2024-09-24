c7cb557bb5f5fefed95995fe1a43dd4dd8da9cf0
surazraaz1998 <srraaz1998@gmail.com>
2024-09-24 22:11:46 +0530

    1. A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 2. The add method can handle any amount of numbers.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Initiating git repo with index.js file
+function add(numbers){
+    const numbersArray=numbers.split(',').map(Number);
+    return numbersArray.reduce((sum,num)=>sum+num,0);
+}  // A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 

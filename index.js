c68e6be416608b0be54e4135f85966d2c0874837
surazraaz1998 <srraaz1998@gmail.com>
2024-09-24 22:18:26 +0530

    Optimising the current method of empty string case

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 
 function add(numbers){
+    if (numbers === "") return 0;  // If input is empty then return 0 instead of running the time complexity of reduce method which is O(n);
     const numbersArray=numbers.split(',').map(Number);
     return numbersArray.reduce((sum,num)=>sum+num,0);
-}  // A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 
+}  

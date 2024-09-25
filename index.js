8eb4668966d5034c898651156cc057cd2ce7c015
surazraaz1998 <srraaz1998@gmail.com>
2024-09-25 12:53:10 +0530

    Supporting  different delimiters

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 
 function add(numbers){
     if (numbers === "") return 0; 
-    const numbersArray=numbers.split(/[,|\n]/).map(Number);  // Adding a regular expression to split the numbers string for comma as well as new line.
+    let delimiter = /,|\n/; // Default delimiters: comma and newline
+    if (numbers.startsWith("//")) { // Handling of custom delimiters using regular expresion
+    const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    delimiter = new RegExp(delimiterMatch[1]);
+    numbers = numbers.substring(delimiterMatch[0].length);
+  }
+    const numbersArray=numbers.split(delimiter).map(Number);  
     return numbersArray.reduce((sum,num)=>sum+num,0);
 }  

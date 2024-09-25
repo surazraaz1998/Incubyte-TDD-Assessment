cadee876cde06abfaeac89b4c1394880aef37214
surazraaz1998 <srraaz1998@gmail.com>
2024-09-25 12:23:11 +0530

    Adding a regular expression to split the numbers string for comma as well as new line.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 
 function add(numbers){
-    if (numbers === "") return 0;  // If input is empty then return 0 instead of running the time complexity of reduce method which is O(n);
-    const numbersArray=numbers.split(',').map(Number);
+    if (numbers === "") return 0; 
+    const numbersArray=numbers.split(/[,|\n]/).map(Number);  // Adding a regular expression to split the numbers string for comma as well as new line.
     return numbersArray.reduce((sum,num)=>sum+num,0);
 }  

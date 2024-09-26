864bce72557c7042c005c232d75c3a6aee224d7f
surazraaz1998 <srraaz1998@gmail.com>
2024-09-26 21:33:16 +0530

    Handling for negatives numbers. Calling add with a negative number will throw an exception

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // A simple String calculator which takes  string numbers with comma seperated input and output the sum of all numbers provided in string. 
 function add(numbers){
     if (numbers === "") return 0; 
-    let delimiter = /,|\n/; // Default delimiters: comma and newline
-    if (numbers.startsWith("//")) { // Handling of custom delimiters using regular expresion
+    let delimiter = /,|\n/; 
+    if (numbers.startsWith("//")) { 
     const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
     delimiter = new RegExp(delimiterMatch[1]);
     numbers = numbers.substring(delimiterMatch[0].length);
-  }
+   }
     const numbersArray=numbers.split(delimiter).map(Number);  
+    const negatives = numbersArray.filter(n => n < 0);  // Handling for negatives numbers 
+
+   if (negatives.length > 0) {
+    throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+   }
     return numbersArray.reduce((sum,num)=>sum+num,0);
 }  

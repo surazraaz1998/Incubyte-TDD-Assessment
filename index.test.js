63d1c2fb81a9b62609ec13253649473c6d3a1d3a
surazraaz1998 <srraaz1998@gmail.com>
2024-09-26 21:52:34 +0530

    Adding test to  return the sum of two comma-separated numbers

--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test('should return 0 for an empty string', () => {
 test('should return the number itself if only one number is provided', () => {
     expect(add("1")).toBe(1);
   });
+
+test('should return the sum of two comma-separated numbers', () => {
+    expect(add("1,2")).toBe(3);
+  });

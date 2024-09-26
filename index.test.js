5a92f07260457e8e0e88ba5b1518dc6628b59929
surazraaz1998 <srraaz1998@gmail.com>
2024-09-26 21:51:41 +0530

    Adding test to return the number itself if only one number is provided

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const add = require("../Incubyte-TDD-Assessment/index");
 test('should return 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('should return the number itself if only one number is provided', () => {
+    expect(add("1")).toBe(1);
+  });

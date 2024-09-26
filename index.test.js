6f847d3b8c259f53fc724d4f8f36fbf3d924639f
surazraaz1998 <srraaz1998@gmail.com>
2024-09-26 21:54:52 +0530

    Adding test to handle new lines as well as commas between numbers

--- index.test.js
+++ index.test.js
@@ -15,3 +15,7 @@ test('should return the sum of two comma-separated numbers', () => {
 test('should handle an unknown amount of numbers', () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
+
+test('should handle new lines as well as commas between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });

da19199a01f7418af02bb7dcb1796044990c6a28
surazraaz1998 <srraaz1998@gmail.com>
2024-09-26 21:56:09 +0530

    Adding test to throw an exception when negative numbers are used

--- index.test.js
+++ index.test.js
@@ -23,3 +23,7 @@ test('should handle new lines as well as commas between numbers', () => {
 test('should support custom delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
   });
+
+test('should throw an exception when negative numbers are used', () => {
+    expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+  });
